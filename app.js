4a5ea25f7ab83971f9fc2ed7581754af515aa384
Emme Gray <lunaienuso@libero.it>
2022-06-11 15:09:02 +0200

    Fixed option Id

--- app.js
+++ app.js
@@ -45,34 +45,32 @@ function createBoard () {
     card.setAttribute('data-id', i)
     card.addEventListener('click',flipCard)
     gridDisplay.appendChild(card)
-
-    console.log(card, i);
   }
 }
 createBoard()
 
 function checkMatch() {
   const cards = document.querySelectorAll('img')
-  const optionOneId = cardsChosenIds[0]
-  const optionTwoId = cardsChosenIds[1]
+  const optionOneID = cardsChosenIds[0]
+  const optionTwoID = cardsChosenIds[1]
   console.log('check for match!');
-  if (optionOneID === optionTwoId ) {
+  if (optionOneID === optionTwoID ) {
     alert('You have clicked the same image!')
   }
 
   if (cardsChosen[0] === cardsChosen[1]){
     alert('You found a match!')
-    cards[optionOneId].setAttribute('src', 'images/white.png')
-    cards[optionTwoId].setAttribute('src', 'images/white.png')
-    cards[optionOneId].removeEventListener('click', flipCard)
-    cards[optionTwoId].removeEventListener('click', flipCard)
+    cards[optionOneID].setAttribute('src', 'images/white.png')
+    cards[optionTwoID].setAttribute('src', 'images/white.png')
+    cards[optionOneID].removeEventListener('click', flipCard)
+    cards[optionTwoID].removeEventListener('click', flipCard)
     cardsWon.push(cardsChosen)
 
     const currentScore = document.getElementById('result').textContent
     document.getElementById('result').textContent=currentScore+1
   } else {
-    cards[optionOneId].setAttribute('src', 'images/blank.png')
-    cards[optionTwoId].setAttribute('src', 'images/blank.png')
+    cards[optionOneID].setAttribute('src', 'images/blank.png')
+    cards[optionTwoID].setAttribute('src', 'images/blank.png')
     alert('Sorry try again!')
   }
   cardsChosen = []
